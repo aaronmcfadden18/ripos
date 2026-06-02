feabe99cd18a0a46163c74b428906401f686a30c
McFazzi <mcfazzi@McFazzis-MacBook-Air.local>
2026-06-02 17:13:54 +0100

    Switch to /landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,5 +5,5 @@ export default async function RootPage() {
   const supabase = await createClient()
   const { data: { user } } = await supabase.auth.getUser()
   if (user) redirect('/dashboard-home')
-  else redirect('/home')
+  else redirect('/landing')
 }
