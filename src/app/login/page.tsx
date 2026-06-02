ee246ba6e984e5735c1fbb2d5d6eca17f7641dcf
McFazzi <mcfazzi@McFazzis-MacBook-Air.local>
2026-06-02 16:34:26 +0100

    Clean up all routing

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -42,7 +42,7 @@ export default function LoginPage() {
 
     const { error } = await supabase.auth.signInWithPassword({ email, password })
     if (error) setError(error.message)
-    else router.push('/')
+    else router.push('/dashboard-home')
     setLoading(false)
   }
 
