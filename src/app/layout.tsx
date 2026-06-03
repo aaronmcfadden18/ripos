37b9739a34d111d07f3c10a5309013d52e482274
McFazzi <mcfazzi@McFazzis-MacBook-Air.local>
2026-06-03 17:31:53 +0100

    Add viewport meta for mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,12 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+}
+
 export const metadata: Metadata = {
   title: "RipOS",
   description: "The profit dashboard for Whatnot card breakers",
